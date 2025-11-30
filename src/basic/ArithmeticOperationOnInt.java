a32c6efd00d22a2e920f6626382f9568ee71cac6
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-11-30 18:43:35 +0530

    add new file

---
+++ src/basic/ArithmeticOperationOnInt.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class ArithmeticOperationOnInt {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+
+        double a = 5;
+        double b = 2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}

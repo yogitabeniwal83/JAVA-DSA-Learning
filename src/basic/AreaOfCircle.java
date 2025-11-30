70f82ecab818ab575c64f8cfe62d0784c3c7ce25
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-11-30 18:51:36 +0530

    add new file

---
+++ src/basic/AreaOfCircle.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        // area = pi*r*r
+        double r = 5;
+        double a = 3.14 * r * r;
+        System.out.println(a);
+    }
+}

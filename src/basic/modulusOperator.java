f7dd78b4e41f08770047b2eb5573855099427be4
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-01 18:57:12 +0530

    modulus operator

---
+++ src/basic/modulusOperator.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class modulusOperator {
+    public static void main(String[] args) {
+
+        int a = 6;
+        int b = 4;
+        int c = a%b;
+
+        System.out.println(c);
+
+     }
+}

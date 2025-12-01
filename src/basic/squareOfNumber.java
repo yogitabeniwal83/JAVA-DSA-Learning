288780256e0133a3f9aefa0e56ffa4b6ba1b9632
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-01 18:21:35 +0530

    square of number

---
+++ src/basic/squareOfNumber.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class squareOfNumber {
+    public static void main(String[] args) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+
+        x = sc.nextInt();
+        System.out.println(x*x);
+    }
+}

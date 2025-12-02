b4b04e5c824636f318712a85a187156fc2718754
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-02 18:27:47 +0530

    relational operator

--- src/If_Else/RelationalOperators.java
+++ src/If_Else/RelationalOperators.java
@@ -2,8 +2,8 @@ package If_Else;
 
 public class RelationalOperators {
     public static void main(String[] args) {
-        System.out.println(8>=9);
-        boolean x = (5>3);
+        System.out.println(8 >= 9);
+        boolean x = (5 > 3);
         System.out.println(x);
     }
 }

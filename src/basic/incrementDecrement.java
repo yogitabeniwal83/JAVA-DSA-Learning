cbfd8f2be94a4d35f8399e636b7d3e23ea91dea3
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-02 18:25:45 +0530

    increment and decrement

---
+++ src/basic/incrementDecrement.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class incrementDecrement {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 10;
+        int z = y++;
+        System.out.println(y+" "+z);
+    }
+}

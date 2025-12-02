724ab25ef608ca42971a9a1e3a070c383c2b214e
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-02 18:34:28 +0530

    Odd or Even

---
+++ src/If_Else/OddEven.java
@@ -0,0 +1,16 @@
+package If_Else;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        if(x % 2 == 0)
+            System.out.println("Even Number");
+        else
+            System.out.println("Odd Number");
+    }
+}

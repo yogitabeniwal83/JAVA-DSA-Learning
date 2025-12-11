49cecc25b52f82b02e50d21c2f56bb842c3223e7
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-11 17:14:15 +0530

    decreasing AP series

---
+++ src/Loop/DecreasingAPSeries.java
@@ -0,0 +1,13 @@
+package Loop;
+
+
+public class DecreasingAPSeries {
+    public static void main(String[] args) {
+
+        for(int i=99; i>0; i-=4){
+            System.out.println(i);
+        }
+    }
+
+
+}

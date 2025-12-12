3bf3e231d4a3ba0348741665dd9b24d276810a15
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-12 19:20:48 +0530

    do while loop

---
+++ src/Loop/do_whileLoop.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class do_whileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+}

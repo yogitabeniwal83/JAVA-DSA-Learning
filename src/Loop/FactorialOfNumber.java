34266a47971f03df81d460a757ca508230c3a04a
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-13 11:06:35 +0530

    factorial of number

---
+++ src/Loop/FactorialOfNumber.java
@@ -0,0 +1,18 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class FactorialOfNumber {
+    public static void main(String[] args) {
+
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+
+        int fact = 1;
+        for(int i=1; i<=n; i++){
+            fact *= i;
+        }
+        System.out.println(fact);
+    }
+}

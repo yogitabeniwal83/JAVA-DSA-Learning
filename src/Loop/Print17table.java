a4eef06a8659f3aadcf07cc828b2f7c831f0989f
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-15 09:01:42 +0530

    print 17 table

---
+++ src/Loop/Print17table.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class Print17table {
+    public static void main(String[] args) {
+
+        for(int i=17; i<=170; i=i+17){
+                System.out.println(i);
+        }
+    }
+}

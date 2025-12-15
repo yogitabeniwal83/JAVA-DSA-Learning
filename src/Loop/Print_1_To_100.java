259d6b11bb63f010a452d255378f9a75350676b6
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-15 09:01:57 +0530

    print 1 to 100

---
+++ src/Loop/Print_1_To_100.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class Print_1_To_100 {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}

898e13a1380c08e61fcc5da539c43736c4d244a3
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-19 19:33:36 +0530

    new file

---
+++ src/Loop/question.java
@@ -0,0 +1,15 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class question {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+       for(int i=1; i<=n; i++) {
+           System.out.println("yogita");
+       }
+    }
+}

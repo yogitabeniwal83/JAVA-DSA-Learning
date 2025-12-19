c8ecde5ae2ec320e49a160ee70053263acd7d7ca
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-12-19 19:33:56 +0530

    new file

---
+++ src/Loop/question_1.java
@@ -0,0 +1,19 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class question_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+            if(i != n) {
+                System.out.println(n);
+                n--;
+            }
+        }
+    }
+}

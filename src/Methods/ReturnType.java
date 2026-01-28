997fa730c3f150b4a6d5b404188b7ba92ac0d3bb
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-01-28 16:50:39 +0530

    return type

---
+++ src/Methods/ReturnType.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class ReturnType {
+    public static int prasun(int a){
+        System.out.println("banu");
+        System.out.println("nanda");
+        if(a>0) return 5;    // return -> means khatam
+        else return 10;
+    }
+    public static void main(String[] args) {
+        System.out.println(3+prasun(-8));
+    }
+}

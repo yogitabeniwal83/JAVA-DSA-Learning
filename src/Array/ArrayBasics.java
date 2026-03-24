40bf226e94343ee30157562ea108656e978f468f
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-03-24 20:13:47 +0530

    array basics

---
+++ src/Array/ArrayBasics.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+
+//        method - 1 to repersent aaray
+
+//         int[] x = {3,7,30,25,8,150,40};
+//         // indexing
+//        System.out.println(x[5]);
+//        // updating elements  -> mutability
+//        x[3] = 45;
+//        System.out.println(x[3]);
+
+
+//        method - 2 to repersent array
+
+        int[] arr = new int[4];  // 4 size ka aaray means indexing from 0 to 3
+        arr[0] = 10;
+        arr[1] = 5;
+        arr[2]= -30;
+        arr[3] = 50;
+
+
+    }
+}

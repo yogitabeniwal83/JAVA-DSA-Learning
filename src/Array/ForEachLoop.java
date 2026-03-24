cd10a8a40c27a78de0c1132048127efe8e80af5a
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-03-24 20:17:27 +0530

    for each loop

---
+++ src/Array/ForEachLoop.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr = {5,8,2,6,9};
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
+
+// for each loop is generally used to traverse array
+// and with the help of for each we can not change the element value of array

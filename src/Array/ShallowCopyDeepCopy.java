3a4e89e5976fd7f51b7e70c4b47483d8c8aa5059
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-04-01 08:51:10 +0530

    shallow copy deep copy

---
+++ src/Array/ShallowCopyDeepCopy.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ShallowCopyDeepCopy {
+    public static void main(String[] args) {
+//         int[] arr = {10,20,30,40};
+//         int[] x = arr;  // x is shallow copy of arr
+//        x[0] = 100;
+//        System.out.println(arr[0]);
+
+        int[] arr = {10,20,30,40};
+        int[] y = Arrays.copyOf(arr,arr.length);  // deep copy
+        y[0] = 100;
+        System.out.println(y[0]);
+        System.out.println(arr[0]);
+    }
+}

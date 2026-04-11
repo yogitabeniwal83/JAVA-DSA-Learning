84e55eb35da27cdf694893dd05fecd303c1c776a
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-04-11 08:59:55 +0530

    triangle

---
+++ src/Pattern/Star/Triangle.java
@@ -0,0 +1,24 @@
+package Pattern.Star;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row Number");
+        int row = sc.nextInt();
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+*
+* *
+* * *
+* * * *
+ */

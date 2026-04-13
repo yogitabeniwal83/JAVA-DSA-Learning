e68528c564deb13167fa4dcd0bdc3a106cb59571
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-04-13 21:03:23 +0530

    number

---
+++ src/Pattern/Numbers/Number.java
@@ -0,0 +1,18 @@
+package Pattern.Numbers;
+
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}

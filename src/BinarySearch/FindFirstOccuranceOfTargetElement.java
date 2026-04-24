0e838751e3544e281156227656db68ac0ddd4102
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-04-24 08:22:58 +0530

    find first occurance occurance of target element

---
+++ src/BinarySearch/FindFirstOccuranceOfTargetElement.java
@@ -0,0 +1,50 @@
+package BinarySearch;
+
+import java.util.Scanner;
+
+public class FindFirstOccuranceOfTargetElement {
+
+   public static void firstOccurance(int[] arr, int target){
+       int left = 0;
+       int right = arr.length-1;
+
+       int index = -1;
+       while(left <= right){
+           int mid = (left+right)/2;
+           if(arr[mid] == target){
+               index = mid;
+               right = mid-1;
+           }
+           else if(arr[mid] < target){
+               left = mid+1;
+           }
+           else{
+               right = mid-1;
+           }
+       }
+
+       if(index != -1){
+           System.out.println("First occurance at index: "+ index);
+       }
+       else {
+           System.out.println("Element not found in the array.");
+       }
+   }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter element of array");
+        for(int i=0; i<n; i++){
+            arr[i]  = sc.nextInt();
+        }
+        System.out.println("Enter target element");
+        int target = sc.nextInt();
+
+        firstOccurance(arr, target);
+    }
+}
